8ee03060abc7b613625ff50b644179c7740adfc8
Santeri Siiranen <santeri.siiranen@gmail.com>
2018-06-17 21:42:37 +0300

    Add icons and fill pwa requirements

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,5 +16,20 @@ module.exports = {
             },
             {text:"Osallistu kehittämiseen", link:"/osallistu/"}
         ]
-    }
+    },
+
+    head: [
+        ["link", {rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png"}],
+        ["link", {rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png"}],
+        ["link", {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"}],
+        ["link", {rel:"manifest", href:"/site.webmanifest"}],
+        ["link", {rel:"mask-icon", href:"/safari-pinned-tab.svg", color:"#5bbad5"}],
+        ["meta", {name:"msapplication-TileColor", content:"#da532c"}],
+        ["meta", {name:"theme-color", content:"#b4dde6"}],
+
+        ["meta", {name:"mobile-web-app-capable", content:"yes"}],
+        ["meta", {name:"apple-mobile-web-app-capable", content:"yes"}],
+        ["meta", {name:"application-name", content:"Norma"}],
+        ["meta", {name:"apple-mobile-web-app-title", content:"Norma"}]
+    ]
 };

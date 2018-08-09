9b751c746581b6a6927e7607f5c89d71de9d309a
Santeri Siiranen <santeri.siiranen@gmail.com>
2018-08-09 23:22:31 +0300

    Refresh icon versions and correct theme color

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -42,13 +42,14 @@ module.exports = {
     },
 
     head: [
-        ["link", {rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png"}],
-        ["link", {rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png"}],
-        ["link", {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"}],
-        ["link", {rel:"manifest", href:"/site.webmanifest"}],
-        ["link", {rel:"mask-icon", href:"/safari-pinned-tab.svg", color:"#3eaf7c"}],
+        ["link", {rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png?v=bOMOYElOBj"}],
+        ["link", {rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png?v=bOMOYElOBj"}],
+        ["link", {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png?v=bOMOYElOBj"}],
+        ["link", {rel:"shotcut icon", href:"/favicon.ico?v=bOMOYElOBj"}],
+        ["link", {rel:"manifest", href:"/site.webmanifest?v=bOMOYElOBj"}],
+        ["link", {rel:"mask-icon", href:"/safari-pinned-tab.svg?v=bOMOYElOBj", color:"#3eaf7c"}],
         ["meta", {name:"msapplication-TileColor", content:"#da532c"}],
-        ["meta", {name:"theme-color", content:"#3eaf7c"}],
+        ["meta", {name:"theme-color", content:"#ffffff"}],
 
         ["meta", {name:"mobile-web-app-capable", content:"yes"}],
         ["meta", {name:"apple-mobile-web-app-capable", content:"yes"}],

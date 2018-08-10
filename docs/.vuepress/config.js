1a17e5f931ce6fb01d5f33d92ca419d20671153e
Santeri Siiranen <santeri.siiranen@gmail.com>
2018-08-10 11:46:25 +0300

    Fix serviceworker popup locale placement

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-module.exports = {    
+module.exports = {  
     locales: {
         "/": {
             lang: "fi",
@@ -20,13 +20,7 @@ module.exports = {
         repoLabel: 'Github',
         docsRepo: 'siikanen/norma',
         docsDir: 'docs',
-        docsBranch: 'master',
-
-        serviceWorker: {
-            updatePopup: { 
-               message: "Uutta sisältöä saatavilla", 
-               buttonText: "Päivitä sivu" 
-             }
+        docsBranch: 'master'
         },
 
         locales: {
@@ -43,10 +37,16 @@ module.exports = {
                         ]
                     },
                     {text:"Osallistu kehittämiseen", link:"/osallistu/"}
-                ]
+                ],
+
+                serviceWorker: {
+                    updatePopup: { 
+                       message: "Uutta sisältöä saatavilla", 
+                       buttonText: "Päivitä sivu" 
+                       }
+                },
             },
         },
-    },
 
     head: [
         ["link", {rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png?v=bOMOYElOBj"}],
